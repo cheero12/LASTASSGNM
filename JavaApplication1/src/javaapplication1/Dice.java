972706a898883cac5eb327c85fd52d5b82ac22d5
KIRAN RANA <30486962+cheero12@users.noreply.github.com>
2017-10-05 14:41:03 +1100

    first upload

---
+++ JavaApplication1/src/javaapplication1/Dice.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+public class Dice {
+			
+	private DiceValue value;
+	
+	public Dice() {
+		value =  DiceValue.getRandom();
+	}
+	
+	public DiceValue getValue() {
+		return value;
+	}
+
+	public DiceValue roll() {
+		return DiceValue.getRandom();
+	}		
+	
+	public String toString() {
+		return value.toString();
+	}
+}
